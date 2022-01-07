aece415c00b3849bfd533e6e1d21d7e8b1652fa5
kronosGR <geo.elgeo@gmail.com>
2022-01-07 20:57:48 +0100

    modularized schemas

---
+++ README.md
@@ -0,0 +1,6 @@
+# Node.js - Express and GraphQL
+
+* Node.js
+* Express (express, express-grqphql)
+* GraphQL (graphql)
+* GrapQL Tools (@graphql-tools/schema, @graphql-tools/load-files)
---
+++ orders/orders.model.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    date: '2005-05-05',
+    subtotal: 90.22,
+    item: [
+      {
+        product: {
+          id: 'redshoe',
+          description: 'Old red Shore',
+          price: 45.11,
+        },
+        quantity: 2,
+      },
+    ],
+  },
+];
