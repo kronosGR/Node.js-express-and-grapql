f5d5e93caaba822987c8da0f30a8322d5df38a50
kronosGR <geo.elgeo@gmail.com>
2022-01-08 11:25:07 +0100

    added filtering in resolvers

--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -1,9 +1,12 @@
-const productsModel = require('products.model');
+const productsModel = require('./products.model');
 
 module.exports = {
   Query: {
     products: () => {
       return productsModel.getAllProducts();
+    },
+    productsByPrice: (_, args) => {
+      return productsModel.getAllProducts(args.min, args.max);
     },
   },
 };
